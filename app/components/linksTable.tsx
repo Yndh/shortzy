9b231f7101b16cfe1ff3965d9b7e485085c87a18
Yndh <przybylski-adrian@o2.pl>
2024-01-06 17:38:55 +0100

    Add Sorting by Date

--- app/components/linksTable.tsx
+++ app/components/linksTable.tsx
@@ -1,5 +1,7 @@
 "use client";
 import {
+  faCaretDown,
+  faCaretUp,
   faChevronDown,
   faChevronUp,
   faCopy,
@@ -27,71 +29,71 @@ interface LinkData {
 }
 
 export default function LinkTable({ showActions = false, styles }: LinkTableProps) {
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
   const [data, setData] = useState<LinkData[]>([
-    {
-      linkCode: "",
-      originalLink: "https://www.twitter.com/",
-      clicks: 1313,
-      active: true,
-      date: new Date(),
-    },
-    {
-      linkCode: "",
-      originalLink: "https://www.youtube.com/",
-      clicks: 3623,
-      active: true,
-      date: new Date(),
-    },
-    {
-      linkCode: "",
-      originalLink: "https://www.figma.com/",
-      clicks: 532,
-      active: true,
-      date: new Date(),
-    },
-    {
-      linkCode: "",
-      originalLink: "https://www.github.com/",
-      clicks: 354,
-      active: true,
-      date: new Date(),
-    },
-    {
-      linkCode: "",
-      originalLink: "https://www.yeezy.com/",
-      clicks: 354,
-      active: true,
-      date: new Date(),
-    },
-    {
-      linkCode: "",
-      originalLink:
-        "https://trackerhub.vercel.app/s/1vW-nFbnR02F9BEnNPe5NBejHRGPt0QEGOYXLSePsC1k/best",
-      clicks: 5634,
-      active: false,
-      date: new Date(),
-    },
-    {
-      linkCode: "",
-      originalLink: "https://nextjs.org/",
-      clicks: 5634,
-      active: true,
-      date: new Date(),
-    },
-    {
-      linkCode: "",
-      originalLink: "https://facebook.com/",
-      clicks: 5634,
-      active: true,
-      date: new Date(),
-    },
-    {
-      linkCode: "",
-      originalLink: "https://discord.gg/",
-      clicks: 5634,
-      active: true,
-      date: new Date(),
-    },
+      {
+        linkCode: "",
+        originalLink: "https://www.twitter.com/",
+        clicks: 1313,
+        active: true,
+        date: new Date('2024-01-04T08:00:00'),
+      },
+      {
+        linkCode: "",
+        originalLink: "https://www.youtube.com/",
+        clicks: 3623,
+        active: true,
+        date: new Date('2024-01-05T14:30:00'),
+      },
+      {
+        linkCode: "",
+        originalLink: "https://www.figma.com/",
+        clicks: 532,
+        active: true,
+        date: new Date('2024-01-06T10:45:00'),
+      },
+      {
+        linkCode: "",
+        originalLink: "https://www.github.com/",
+        clicks: 354,
+        active: true,
+        date: new Date('2024-01-04T19:20:00'),
+      },
+      {
+        linkCode: "",
+        originalLink: "https://www.yeezy.com/",
+        clicks: 354,
+        active: true,
+        date: new Date('2024-01-05T09:05:00'),
+      },
+      {
+        linkCode: "",
+        originalLink: "https://trackerhub.vercel.app/s/1vW-nFbnR02F9BEnNPe5NBejHRGPt0QEGOYXLSePsC1k/best",
+        clicks: 5634,
+        active: false,
+        date: new Date('2024-01-06T16:15:00'),
+      },
+      {
+        linkCode: "",
+        originalLink: "https://nextjs.org/",
+        clicks: 5634,
+        active: true,
+        date: new Date('2024-01-04T12:10:00'),
+      },
+      {
+        linkCode: "",
+        originalLink: "https://facebook.com/",
+        clicks: 5634,
+        active: true,
+        date: new Date('2024-01-05T20:55:00'),
+      },
+      {
+        linkCode: "",
+        originalLink: "https://discord.gg/",
+        clicks: 5634,
+        active: true,
+        date: new Date('2024-01-06T06:40:00'),
+      },
   ]);
 
   useEffect(() => {
@@ -134,6 +136,27 @@ export default function LinkTable({ showActions = false, styles }: LinkTableProp
     );
   };
 
+  const toggleMobileList = (index: number) => {
+    () => {
+      const updatedData = data.map((item, idx) => {
+        if (idx === index) {
+          return { ...item, isOpen: !item.isOpen };
+        }
+        return item;
+      });
+      setData(updatedData);
+    }
+  }
+
+  const sortByDate = () => {
+    sortDirection === "asc" ? setSortDirection("desc") : setSortDirection("asc");
+  }
+
+  const getSortedData = () => {
+    const sortedData = [...data].sort((a, b) => a.date.getTime() - b.date.getTime());
+    return sortDirection === "asc" ? sortedData : sortedData.reverse();
+  }
+
   return (
     <div className={styles.tableContainer}>
       <table className={styles.linksTable}>
@@ -144,13 +167,21 @@ export default function LinkTable({ showActions = false, styles }: LinkTableProp
             <th>QR Code</th>
             <th>Clicks</th>
             <th>Status</th>
-            <th>Date</th>
+            <th>
+              <div className={styles.thContainer} onClick={sortByDate}>
+                Date
+                <div className={`${styles.sortContainer} ${(sortDirection === "asc" ? styles.asc : styles.desc)}`}>
+                  <FontAwesomeIcon icon={faCaretUp}/>
+                  <FontAwesomeIcon icon={faCaretDown}/>
+                </div> 
+              </div> 
+            </th>
             {showActions && <th>Actions</th>}
           </tr>
         </thead>
         <tbody>
           {data.length > 0 ? (
-            data.map((row, index) => {
+            getSortedData().map((row, index) => {
               return (
                 <tr key={index} className={row.isOpen ? styles.active : ""}>
                   <td>
@@ -173,15 +204,7 @@ export default function LinkTable({ showActions = false, styles }: LinkTableProp
                       </button>
                       <button
                         className={styles.mobileButton}
-                        onClick={() => {
-                          const updatedData = data.map((item, idx) => {
-                            if (idx === index) {
-                              return { ...item, isOpen: !item.isOpen };
-                            }
-                            return item;
-                          });
-                          setData(updatedData);
-                        }}
+                        onClick={() => toggleMobileList(index)}
                       >
                         <FontAwesomeIcon
                           icon={row.isOpen ? faChevronUp : faChevronDown}
@@ -220,11 +243,11 @@ export default function LinkTable({ showActions = false, styles }: LinkTableProp
                     <td>
                       <div style={{display: "flex", gap: 7}}>
                         <button className={styles.tableButton}>
-                          <FontAwesomeIcon icon={faTrash} />
-                        </button>
-
-                        <button className={styles.tableButton}>
                           <FontAwesomeIcon icon={faPen} />
+                        </button>
+                        
+                        <button className={styles.tableButton}>
+                          <FontAwesomeIcon icon={faTrash} />
                         </button>
                       </div>
                     </td>
