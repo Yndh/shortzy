34c4db224642a39adda3ec985db0da58d8441964
Yndh <przybylski-adrian@o2.pl>
2024-01-14 19:56:50 +0100

    Add session validation in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,8 +3,14 @@ import LinksTable from "../components/linksTable";
 import Header from "../components/header";
 import SwitchToggleButton from "../components/switchToggleButton";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <main className={styles.main}>
       <Header shortener={true} styles={styles} />
