84dcb037eecf3a018bc4fcaa5ac555c95850b568
Yndh <przybylski-adrian@o2.pl>
2024-01-18 12:11:01 +0100

    Add redirect error page

--- app/[code]/page.tsx
+++ app/[code]/page.tsx
@@ -2,9 +2,12 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import styles from "@/app/notFound.module.scss";
+import Link from "next/link";
 
 export default function LinkRedirect({ params }: { params: { code: string } }) {
   const [originalUrl, setOriginalUrl] = useState<string | null>(null);
+  const [error, setError] = useState(false);
 
   const router = useRouter();
 
@@ -22,13 +25,29 @@ export default function LinkRedirect({ params }: { params: { code: string } }) {
           })
           .catch((error) => {
             console.error("Error fetching data:", error);
+            setError(true);
           });
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
+        setError(true);
       });
     console.log("a");
   }, []);
 
-  return <>{originalUrl && <p>Redirecting to: {originalUrl}</p>}</>;
+  return (
+    <>
+      {error ? (
+        <div className={styles.main}>
+          <h1>Oops!</h1>
+          <p>It seems like this link is having a bit of a day off!</p>
+          <Link href="/">
+            <button>Let's go Home</button>
+          </Link>
+        </div>
+      ) : (
+        originalUrl && <p>Redirecting to: {originalUrl}</p>
+      )}
+    </>
+  );
 }
