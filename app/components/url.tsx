1dc329c6fab81af842342f6787b7a5bdaa516c7c
Yndh <przybylski-adrian@o2.pl>
2024-02-20 16:58:36 +0100

    Fix getUrl function

--- app/components/url.tsx
+++ app/components/url.tsx
@@ -1,14 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 export default function getUrl() {
-  const [baseUrl, setBaseUrl] = useState("");
-
-  useEffect(() => {
-    const { protocol, host } = window.location;
-    setBaseUrl(`${protocol}//${host}`);
-  }, []);
-
-  return baseUrl;
+  const { protocol, host } = window.location;
+  return `${protocol}//${host}`;
 }
